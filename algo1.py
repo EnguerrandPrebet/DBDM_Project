2215d7fee65dcb4d8e7163fc5a1a136fcb56e83f
EnguerrandPrebet <enguerrand.prebet@ens-lyon.fr>
2017-03-22 12:18:28 +0100

    Implementation of "improved". Change to SetAttr
    - SetAttr has now a hash, and should receive only string (or never
    asking to print it)
    
    - First algorithm renamed (as naive)
    
    - choose strategy must be discussed
    
    - beginning of improved should be in a different function (according to
    the subject)

--- algo1.py
+++ algo1.py
@@ -1,10 +1,40 @@
-def closure(fds,attr):
-    cl = SetAttr(attr)
-    done = False
-    while(not done):
-        done = True
-        for fd in fds:
-            if(fds.prerequis.issubset(cl) and not fds.conclusion.issubset(cl)):
-                cl = cl.union(fds.prerequis)
-                done = False
-    return cl
+def naive(fds,attr):
+	cl = SetAttr(attr)
+	done = False
+	while(not done):
+		done = True
+		for fd in fds:
+			if(fds.prerequis.issubset(cl) and not fds.conclusion.issubset(cl)):
+				cl = cl.union(fds.prerequis)
+				done = False
+	return cl
+	
+
+	
+def choose(u):
+	return u.pop()
+		
+def improved(fds,attr):
+	count = dict()
+	list = dict()
+	already_added = set()
+	for fd in fds:
+		count[fd] = len(fd.prerequis)
+		for attribute in fd.prerequis:
+			if attribute in already_added:
+				list[attribute].append(fd)
+			else:
+				list[attribute] = []
+				
+	closure = SetAttr(attr)
+	update = SetAttr(attr)
+	while update:
+		a = choose(update)
+		update.remove(a)
+		for fd in list[a]:
+			count[fd] -= 1
+			if(count[fd] == 0):
+				update.union(fd.conclusion.difference(closure))
+				closure.union(fd.conclusion)
+	return closure
+	
--- setatt.py
+++ setatt.py
@@ -1,4 +1,15 @@
 class SetAttr(set):
-    def __repr__(self):
-        s = set.__repr__(self)
-        return "".join(s[len("SetAttr({"):-len("})")].split(", "))
+	def __init__(self, *args, **kwargs):
+		super(SetAttr,self).__init__(*args,**kwargs)
+		self.s_hash = hash(str(self))
+		
+	def __repr__(self):
+		s = set.__repr__(self)
+		return "".join(list(self))
+	
+	def __hash__(self):
+		return self.s_hash
+		
+	def upd_hash(self):
+		self.s_hash = hash(str(self))
+		
